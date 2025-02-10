a257013db222db3fa43527d84a03ee4802b9c1bf
Tobias Kragholm <50784887+tkragholm@users.noreply.github.com>
2025-02-10 10:51:00 +0100

    fixed setup.py

--- setup.py
+++ setup.py
@@ -5,20 +5,20 @@ from setuptools import find_packages, setup
 # Read version from __version__.py
 about = {}
 here = Path(__file__).parent
-with open(here / "readstat_rs_dist" / "__version__.py", encoding="utf-8") as f:
+with open(here / "readstat_rs" / "__version__.py", encoding="utf-8") as f:
     exec(f.read(), about)
 
 setup(
     name="readstat_rs_dist",
     version=about["__version__"],
-    packages=find_packages(include=["readstat_rs_dist", "readstat_rs_dist.*"]),
+    packages=find_packages(include=["readstat_rs", "readstat_rs.*"]),
     package_data={
-        "readstat_rs_dist": ["bin/*"],
+        "readstat_rs": ["bin/*"],
     },
     include_package_data=True,
     entry_points={
         "console_scripts": [
-            "readstat=readstat_rs_dist.entry_point:main",
+            "readstat=readstat_rs.entry_point:main",
         ],
     },
     python_requires=">=3.7",
